709b6d3a689406aac2afd25ac2a692ac6b6511e6
RicardoGoncal <ricardo.studo@gmail.com>
2023-08-12 16:44:57 -0300

    Development cloud Form HTML

--- cloud/cloud.py
+++ cloud/cloud.py
@@ -1,9 +1,38 @@
-# from flask.views import MethodView
-# from flask_smorest import abort, Blueprint
-# from schemas import CloudSchema
-# from models import CloudModel
-# from sqlalchemy.exc import SQLAlchemyError
-# from db import db
+from flask.views import MethodView
+from flask_smorest import abort, Blueprint
+from flask import render_template, request, url_for
+from schemas import CloudSchema
+from models import CloudModel
+from sqlalchemy.exc import SQLAlchemyError
+from db import db
 
-# blp = Blueprint("Cloud", __name__, description="Operações para Disciplina Cloud")
+blp = Blueprint("Cloud", __name__, template_folder='templates' )
 
+@blp.route('/cloud_form')
+def get():
+    return render_template("cloud/cloud_view.html")
+
+
+@blp.route('/processar_form', methods=['POST'])
+def processar_form():
+
+    form = CloudModel(**dict(request.form))
+
+    # form = CloudModel(**form)
+
+    try:
+        print(form)
+        db.session.add(form)
+        db.session.commit()
+    except Exception as e:
+        db.session.rollback()
+        print(e)
+
+    return "retornar a pagina do formulario ou a inicial"
+
+
+@blp.route('/ver_bd')
+class VerBd(MethodView):
+    @blp.response(200, CloudSchema(many=True)) # Decorator
+    def get(self):
+        return CloudModel.query.all()
--- models/cloud.py
+++ models/cloud.py
@@ -5,9 +5,9 @@ class CloudModel(db.Model):
     __tablename__ = "cloud"
 
     request_id = db.Column(db.Integer, primary_key=True)
-    nome_requisitor = db.Column(db.String(80), unique=True, nullable=False)
-    disciplina = db.Column(db.String(80), unique=True, nullable=False)
-    nome_do_rg = db.Column(db.String(80), unique=True, nullable=False)
+    nome_requisitor = db.Column(db.String(80), nullable=False)
+    disciplina = db.Column(db.String(80), nullable=False)
+    nome_do_rg = db.Column(db.String(80), nullable=False)
   
 
 
