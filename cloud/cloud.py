9768496b9b504e1b89392bb0ce52ed3e7ed4ab87
RicardoGoncal <ricardo.studo@gmail.com>
2023-08-19 17:12:25 -0300

    Adjust HTML and test insert in database

--- cloud/cloud.py
+++ cloud/cloud.py
@@ -1,7 +1,6 @@
 from flask.views import MethodView
 from flask_smorest import abort, Blueprint
 from flask import render_template, request, url_for
-from schemas import CloudSchema
 from models import CloudModel
 from sqlalchemy.exc import SQLAlchemyError
 from db import db
@@ -20,6 +19,34 @@ def processar_form():
 
     # form = CloudModel(**form)
 
+    # print(form.inputPPM)
+    # print(form.inputTypeCust)
+    # print(form.inputProjectDemandName)
+    # print(form.inputDemandPEPCC)
+    # print(form.inputManager)
+    # print(form.inputArchitect)
+    # print(form.inputEntityResposible)
+    # print(form.inputItOt)
+    # print(form.inputSpecialReq)
+    # print(form.inputHelpText)
+    # print(form.radioRg)
+
+    # print(form.inputResourceGroup )
+    # print(form.inputNeedUsersAccess )
+    # print(form.inputProjectName )
+    # print(form.inputResponsible )
+    # print(form.inputWorkType )
+    # print(form.inputRegion )
+    # print(form.radioTransition )
+    # print(form.inputDate )
+
+    # print(form.radioSp)
+    # print(form.inputHLD)
+    # print(form.inputFunctionArea)
+    # print(form.inputAzureResources)
+    # print(form.radioNet)
+    # print(form.inputDescribeConfig)
+
     try:
         print(form)
         db.session.add(form)
@@ -31,8 +58,8 @@ def processar_form():
     return "retornar a pagina do formulario ou a inicial"
 
 
-@blp.route('/ver_bd')
-class VerBd(MethodView):
-    @blp.response(200, CloudSchema(many=True)) # Decorator
-    def get(self):
-        return CloudModel.query.all()
+# @blp.route('/ver_bd')
+# class VerBd(MethodView):
+#     @blp.response(200, CloudSchema(many=True)) # Decorator
+#     def get(self):
+#         return CloudModel.query.all()
--- models/cloud.py
+++ models/cloud.py
@@ -4,12 +4,42 @@ from db import db
 class CloudModel(db.Model):
     __tablename__ = "cloud"
 
-    request_id = db.Column(db.Integer, primary_key=True)
-    nome_requisitor = db.Column(db.String(80), nullable=False)
-    disciplina = db.Column(db.String(80), nullable=False)
-    nome_do_rg = db.Column(db.String(80), nullable=False)
-  
+    # request_id = db.Column(db.Integer, primary_key=True)
+    # nome_requisitor = db.Column(db.String(80), nullable=False)
+    # disciplina = db.Column(db.String(80), nullable=False)
+    # nome_do_rg = db.Column(db.String(80), nullable=False)
+    
+    # Campos comuns parte 1 - Antes da escolha do radioRg
+    requestId = db.Column(db.Integer, primary_key=True)
+    inputPPM = db.Column(db.Integer, nullable=True)
+    inputTypeCust = db.Column(db.String(80), nullable=True)
+    inputProjectDemandName = db.Column(db.String(80), nullable=True)
+    inputDemandPEPCC = db.Column(db.String(80), nullable=True)
+    inputManager = db.Column(db.String(80), nullable=True)
+    inputArchitect = db.Column(db.String(80), nullable=True)
+    inputEntityResposible = db.Column(db.String(80), nullable=True)
+    inputItOt = db.Column(db.String(80), nullable=True)
+    inputSpecialReq = db.Column(db.String(80), nullable=True)
+    inputHelpText = db.Column(db.Text, nullable=True)
+    radioRg = db.Column(db.String(25), nullable=True)
 
+    # Campos que serão preenchidos dependendo do resultado do radioRg(Existe ou nao um RG)
+    inputResourceGroup = db.Column(db.String(80), nullable=True)
+    inputNeedUsersAccess = db.Column(db.String(80), nullable=True)
+    inputProjectName = db.Column(db.String(80), nullable=True)
+    inputResponsible = db.Column(db.String(80), nullable=True)
+    inputWorkType = db.Column(db.String(80), nullable=True)
+    inputRegion = db.Column(db.String(80), nullable=True)
+    radioTransition = db.Column(db.String(50), nullable=True)
+    inputDate = db.Column(db.Date, nullable=True)
+
+    # Campos comuns parte 2 - Apos a escolha do radioRg
+    radioSp = db.Column(db.String(25), nullable=True)
+    inputHLD = db.Column(db.String(80), nullable=True)
+    inputFunctionArea = db.Column(db.String(80), nullable=True)
+    inputAzureResources = db.Column(db.String(80), nullable=True)
+    radioNet = db.Column(db.String(25), nullable=True)
+    inputDescribeConfig = db.Column(db.Text, nullable=True)
 
 
 
